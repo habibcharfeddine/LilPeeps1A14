126bdc3257f3eada849e79f63b429e3f0efed7ac
inessfar1 <49401548+inessfar1@users.noreply.github.com>
2019-05-06 08:18:14 +0100

    Add files via upload

---
+++ Habib Charfeddine (Final)/animation.h
@@ -0,0 +1,12 @@
+
+#include "structs.h"
+
+/* Structures globales */
+extern Gestion jeu;
+extern GameObject player;
+extern GameObject player2;
+extern Map map;
+
+
+/* Prototypes */
+extern SDL_Surface *loadImage(char *name);
---
+++ Habib Charfeddine (Final)/animation.h~
@@ -0,0 +1,11 @@
+
+#include "structs.h"
+
+/* Structures globales */
+extern Gestion jeu;
+extern GameObject player;
+extern Map map;
+
+
+/* Prototypes */
+extern SDL_Surface *loadImage(char *name);
